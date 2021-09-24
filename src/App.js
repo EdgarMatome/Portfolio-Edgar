68ee1e8cea7bbfc18321d947a0ddf519f61e7aa8
EdgarMatome <edgarmurudu@gmail.com>
2021-09-24 22:17:07 +0200

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from 'react-particles-js';
 import Nevbar from "./components/Nevbar";
+import Header from "./components/Header";
 
 function App() {
   return (
+    <>
+    <Particles
+    className="particles-canvas"
+      params={{
+        Particles:{
+          Number:{
+            value:5,
+            density:{
+              enable:true,
+              value_area:900
+            }
+          },
+          shape:{
+            type: "star",
+            stroke:{
+              width: 6,
+              color:"#f9ab00"
+            }
+          }
+        }
+      }}
+    />
     <Nevbar/>
+    <Header/>
+    </>
   );
 }
 
