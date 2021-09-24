93c9eebc8ee636e88f4559347b20ba78464b3263
EdgarMatome <edgarmurudu@gmail.com>
2021-09-24 15:56:26 +0200

    Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Nevbar from "./components/Nevbar";
 
 function App() {
   return (
-<h2>Edgar Online</h2>
+    <Nevbar/>
   );
 }
 
